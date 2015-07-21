ac6f6b9d507de40054dd6c64f7d711fbf8d7d594
Uchio KONDO <udzura@pepabo.com>
2015-07-21 11:29:55 +0900

    Cloning redis plugin

--- recipe.rb
+++ recipe.rb
@@ -22,3 +22,13 @@ execute "install dokku" do
   cwd '/var/tmp/dokku'
   not_if 'which dokku'
 end
+
+git "/var/lib/dokku/plugins/redis" do
+  repository "https://github.com/luxifer/dokku-redis-plugin"
+  notify "execute[dokku plugins-install]"
+end
+
+execute "dokku plugins-install" do
+  action :nothing
+  cwd "/var/lib/dokku/plugins"
+end
